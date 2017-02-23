913668e952d51c67fe02d56808010e0dcd9b8eb0
pepsik-kiev <pepsik@ukr.net>
2017-02-23 21:24:24 +0200

    Remove locale & add world-flags-sprite css

--- plugins/stat_plugin.py
+++ plugins/stat_plugin.py
@@ -8,7 +8,6 @@ import time
 import logging
 import urllib2
 import plugins.modules.ipaddr as ipaddr
-import locale
 import json
 
 localnetranges = (
@@ -22,7 +21,6 @@ localnetranges = (
 
 class Stat(AceProxyPlugin):
     handlers = ('stat', 'favicon.ico')
-    locale.setlocale(locale.LC_ALL, locale.getdefaultlocale())
     logger = logging.getLogger('STAT')
 
     def __init__(self, AceConfig, AceStuff):
@@ -30,15 +28,15 @@ class Stat(AceProxyPlugin):
         self.stuff = AceStuff
 
     def geo_ip_lookup(self, ip_address):
-#        lookup_url = 'http://api.2ip.ua/geo.json?ip=' + ip_address
-        lookup_url = 'http://freegeoip.net/json/'+ip_address
+        lookup_url = 'http://freegeoip.net/json/' + ip_address
         Stat.logger.debug('Trying to obtain geoip info : ' + lookup_url)
 
         req = urllib2.Request(lookup_url, headers={'User-Agent' : "Magic Browser"})
         response = json.loads(urllib2.urlopen(req, timeout=10).read())
 
-        return {'country' : '-/-' if not response['country_name'] else response['country_name'] ,
-                'city'    : '-/-' if not response['city'] else response['city']}
+        return {'country_code' : '' if not response['country_code'] else response['country_code'] ,
+                'country'      : '' if not response['country_name'] else response['country_name'] ,
+                'city'         : '' if not response['city'] else response['city']}
 
     def handle(self, connection, headers_only=False):
         current_time = time.time()
@@ -54,13 +52,16 @@ class Stat(AceProxyPlugin):
         if headers_only:
             return
         
-        connection.wfile.write(
-            '<meta http-equiv="Refresh" content="60" />'
-            '<html><body><h4>Connected clients: ' + str(self.stuff.clientcounter.total) + '</h4>')
-        connection.wfile.write(
-            '<h5>Concurrent connections limit: ' + str(self.config.maxconns) + '</h5><table  border="2" cellspacing="0" cellpadding="3">')
-        connection.wfile.write(
-            '<tr align=CENTER valign=BASELINE BGCOLOR="#C0C0C0"><td>Channel name</td><td>Client IP</td><td>Location</td><td>Start time</td><td>Duration</td></tr>')
+        connection.wfile.write('<html><head>')
+        connection.wfile.write('<meta charset="UTF-8" http-equiv="Refresh" content="60"/>')
+        connection.wfile.write('<title>AceProxy stat info</title>')
+        connection.wfile.write('<link rel="stylesheet" type="text/css" href="http://cloud.github.com/downloads/lafeber/world-flags-sprite/flags16.css"/>')
+        connection.wfile.write('<link rel="shortcut icon" href="http://savepic.ru/13036690.png" type="image/png">')
+        connection.wfile.write('</head>') 
+        connection.wfile.write('<body><div class="f16"><h4>Connected clients: ' + str(self.stuff.clientcounter.total) + '</h4>')
+        connection.wfile.write('<h5>Concurrent connections limit: ' + str(self.config.maxconns) + '</h5><table  border="2" cellspacing="0" cellpadding="3">')
+        connection.wfile.write('<tr align=CENTER valign=BASELINE BGCOLOR="#eeeee5"><td>Channel name</td><td>Client IP</td><td>Location</td><td>Start time</td><td>Duration</td></tr>')
+
         for i in self.stuff.clientcounter.clients:
             for c in self.stuff.clientcounter.clients[i]:
                 connection.wfile.write('<tr><td>')
@@ -75,11 +76,10 @@ class Stat(AceProxyPlugin):
                 clientinrange = any(map(lambda i: ipaddr.IPAddress(c.handler.clientip) in ipaddr.IPNetwork(i),localnetranges))
 
                 if clientinrange:
-                    connection.wfile.write('<td>' + 'Local IP adress' + '</td>')
+                    connection.wfile.write('<td>' + 'Local IP adress ' + '</td>')
                 else:
                     geo_ip_info = self.geo_ip_lookup(c.handler.clientip)
-                    connection.wfile.write('<td>' + geo_ip_info.get('country').encode('utf-8') + ', ' + geo_ip_info.get('city').encode('utf-8') + '</td>')
-
+                    connection.wfile.write('<td>' + geo_ip_info.get('country').encode('UTF8') + ', ' + geo_ip_info.get('city').encode('UTF8') + ' ' + '<i class="flag ' + geo_ip_info.get('country_code').encode('UTF8').lower() + '"></i></td>')
                 connection.wfile.write('<td>' + time.strftime('%c', time.localtime(c.connectionTime)) + '</td>')
                 connection.wfile.write('<td align="center">' + time.strftime("%H:%M:%S",  time.gmtime(current_time-c.connectionTime)) + '</td></tr>')
-        connection.wfile.write('</table></body></html>')
+        connection.wfile.write('</table></div></body></html>')
